6497ca5ab0730f76fd3c20d9c6b49a1ef7bef868
DivinePastry <browning.365@osu.edu>
2023-09-19 22:46:12 -0400

    First commit? who knows git bash isn't cooperating

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,12 @@
 print("hello world")
+
+#X marks the spot
+#I spent more time on this than i'd like to admit
+for i in range (8):
+    for j in range (8):
+        if (i == j or  8-(j+1) == i):
+            print("*", end= "")
+        else:
+            print("     ", end= "")
+    print("\n")
+
